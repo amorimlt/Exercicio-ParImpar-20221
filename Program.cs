8a6a86271eaa61e97bc3e0118f5ddddf88f2922a
amorimlt <julianeamorim.lt@gmail.com>
2022-04-01 20:42:55 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int Numerodigitado;
+
+Console.Write("\nDigite um número: ");
+Numerodigitado = Convert.ToInt32 (Console.ReadLine());
+
+bool NumeroEhPar = (Numerodigitado % 2) == 0;
+
+if (NumeroEhPar)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine ("Esse número é par, divisível por 2\n");
+    Console.ResetColor();
+
+}
+
+else 
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine ("Esse número é ímpar, não divisível por 2\n");
+    Console.ResetColor();
+}
+
+//Console.ResetColor();
